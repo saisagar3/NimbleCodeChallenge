2516aec0cdb33cd76526350fbf4919c260512011
Sai Sagar <sagar.kachibatla@gmail.com>
2019-04-12 19:26:49 +0700

    Added Role column to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  enum role: {'Instructor': 0, 'Student': 1, 'Others': 2}
 end
