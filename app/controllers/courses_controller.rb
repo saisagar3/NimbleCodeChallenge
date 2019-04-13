0bc5cee43aeaf062358aef51af562b4d8a06937d
Sai Sagar <sagar.kachibatla@gmail.com>
2019-04-13 20:25:44 +0700

    Added associations between User and Course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -25,7 +25,7 @@ class CoursesController < ApplicationController
   # POST /courses.json
   def create
     @course = Course.new(course_params)
-
+    @course.user_id = current_user.id
     respond_to do |format|
       if @course.save
         format.html { redirect_to @course, notice: 'Course was successfully created.' }
@@ -69,6 +69,6 @@ class CoursesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
-      params.require(:course).permit(:course_name, :description, :start_date, :end_date)
+      params.require(:course).permit(:course_name, :description, :start_date, :end_date, :user_id)
     end
 end
