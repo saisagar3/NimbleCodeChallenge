59b33a71e3c802b0fd082cf5c4529d840b6262b0
Sai Sagar <sagar.kachibatla@gmail.com>
2019-04-17 16:03:42 +0700

    Extended the Devise controllers methods instead of ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,8 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
   protected
   
-  def configure_permitted_parameters
-	  devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :citizen_id, :role, :institute_id, :email, :password, :remember_me])
-	  devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password, :remember_me])
-	  devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :citizen_id, :role, :institute_id])
-  end
-
   def user_not_authorized
     flash[:alert] = "Access Denied."
     redirect_to (request.referrer || root_path)
